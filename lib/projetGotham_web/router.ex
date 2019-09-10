ec4d5f3bccb51b3bae27eb0798d43586798f2de2
AbdelghaniSalhi <55136498+AbdelghaniSalhi@users.noreply.github.com>
2019-09-10 18:04:50 +0200

    router.ex fonctionnel

--- lib/projetGotham_web/router.ex
+++ lib/projetGotham_web/router.ex
@@ -7,6 +7,26 @@ defmodule ProjetGothamWeb.Router do
 
   scope "/api", ProjetGothamWeb do
     pipe_through :api
+  
+    scope"/api" ProjetGothamWeb do
+      get "/", UserController, :index
+      get "/:UserID", UserController :show
+      post "/", UserController, :create
+      put "/:userID", UserController, :update
+      delete "/:userID", UserController, :delete
+    end
+  
+    scope "/clocks" do
+      get "/:userID", ClockController, :index
+      post "/:userID", ClockController, :create
+    end
+  
+    scope "/workingtimes" do
+      get "/:userID", WorkingTimesController, :index
+      get "/:userID/:workingtimeID", WorkingTimesController, :show
+      post "/:userID", WorkingTimesController :create
+      put "/:iD", WorkingTimesController :update
+      delete "/:iD" WorkingTimesController :delete
+    end
   end
-
 end
